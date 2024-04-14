a149d9261e91a5ab87037a31192a9c7878399b11
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-04-15 01:54:42 +0530

    minor update

--- src/Mycompnents/Footer.js
+++ src/Mycompnents/Footer.js
@@ -1,21 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 const Footer = () => {
+  const [currentYear, setCurrentYear] = useState(0);
 
-    let footerStyle = {
-        position : "relative",
-        width : "100%",
-        top : "60vh",
-        borderTop : "2px solid brown"
-    }
+  let footerStyle = {
+    position: "relative",
+    width: "100%",
+    top: "60vh",
+    borderTop: "2px solid brown",
+  };
 
-    return (
-        <>
-            <footer className="bg-dark text-light py-2" style={footerStyle}>
-            <p className="text-center">Copyright &copy; SiddharthaSarkar</p>
-            </footer>
-        </>
-    )
-}
+  useEffect(() => {
+    const currYear = new Date().getFullYear();
+    setCurrentYear(currYear);
+  }, []);
 
-export default Footer; 
+  return (
+    <>
+      <footer className="bg-dark text-light py-2" style={footerStyle}>
+        <p className="text-center">
+          Copyright &copy; {currentYear} <Link to="#"> Siddhartha Sarkar</Link>.
+          All rights reserved.
+        </p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
